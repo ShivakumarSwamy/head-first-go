4829c2eb790756f4f59bc34514d8fe6f3d03cf2f
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2022-06-11 19:42:26 +0530

    chapter 2: page 45: shadowing names
    Signed-off-by: ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>

---
+++ chapter2/page45.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+
+	var int int = 12                               // type
+	var append string = "minutes of bonus footage" // function
+	var fmt string = "DVD"                         // package
+	var count int
+	var languages = append([]string{}, "Español")
+	fmt.Println(int, append, "on", fmt, languages)
+}
