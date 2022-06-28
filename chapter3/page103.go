fdf866aba8c70cab70fcb59f0cbc778bbc261efe
ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>
2022-06-28 07:18:28 +0530

    chapter 3: page 103: pointer
    Signed-off-by: ShivakumarSwamy <27730051+ShivakumarSwamy@users.noreply.github.com>

---
+++ chapter3/page103.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	amount := 6          // initialize a variable
+	fmt.Println(amount)  // print variable value
+	fmt.Println(&amount) // print variable address
+}
